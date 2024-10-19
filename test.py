7e0c35dfbb3abc85423eee27b44b95d04bb7d9ae
Albert Pineda <albertpinedabas@gmail.com>
2024-10-19 18:39:19 +0200

    Scrape script done!

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 from bs4 import BeautifulSoup
-from product import Product
 import json
 import requests
+
+def parseProduct():
+ pass
 
 # get HTML
 resp = requests.get("https://www.optosigma.com/eu_en/optics/lenses/spherical-lenses/plano-convex-spherical-lenses/n-bk7-plano-convex-lenses-ar-400-700nm-SLB-P-M.html")
 
-html = resp.text
 
-soup = BeautifulSoup(html, "html.parser")
+soup = BeautifulSoup(resp.text, "html.parser")
 
 # get only Rows, so we can only access to links
 table = soup.find('tbody', {'class': "grouped-items-body"})
@@ -16,5 +17,34 @@ table = soup.find('tbody', {'class': "grouped-items-body"})
 #get links (<a)
 links = [link.get('href') for link in table.find_all('a')]
 
+#dictionary
+products_dict = {}
+
+#debug purpose only:
+link_count = 0
+
+#treat every link
+for link in links:
+    link_count = link_count + 1
+    print("Treating the " + str(link_count) + " link: "+ link)
+    resp = requests.get(link)
+    soup = BeautifulSoup(resp.text, "html.parser")
+    
+    table = soup.find('table', {'id': 'product-attribute-specs-table'})
+
+    rows = [row for row in table.find_all('tr')]
+
+    product = {}
+    for row in rows:
+       product[row.find('th', {'class':'col label'}).string] = row.find('td', {'class':'col data'}).string
+    
+    code = soup.find('div', {'class': 'product-info-sku'})
+
+    products_dict[code.p.span.string] = product
+
+
+with open("product.json", "w") as json_file:
+    json.dump(products_dict, json_file, indent=4)
+
 # print the HTML as text
-print(links)
+print("Done! Check the json file!")
